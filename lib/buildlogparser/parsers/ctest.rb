361b1383119dd2541f80916900f608a63332893a
Mario Werner <mario.werner@iaik.tugraz.at>
2017-07-24 09:32:42 +0200

    initial log parsing support for ctest

--- lib/buildlogparser/parsers/ctest.rb
+++ lib/buildlogparser/parsers/ctest.rb
@@ -17,36 +17,98 @@ module BuildLogParser
       rule(:float)     { integer >> (match['\.,'] >> integer).maybe }
 
       rule(:event) do
-        space? >> integer.as(:nr) >> str('/') >> integer >> space? >> str('Test') >> space? >> str('#') >>
+        space? >> integer.as(:nr) >> str('/') >> integer.as(:total_nr) >> space? >> str('Test') >> space? >> str('#') >>
         integer >> str(': ') >> letters.as(:name) >> match['\.*\s'].repeat(1) >> letters.as(:result) >> space? >>
-        float.as(:time) >> anyline
+        float.as(:time_sec) >> anyline
       end
 
-      rule(:errors)    { (event | anyline.as(:drop)).repeat.as(:array) }
-      root(:errors)
+      rule(:start)    { (event | anyline.as(:drop)).repeat.as(:array) }
+      root(:start)
     end # class Parser
 
     class Transform < Parslet::Transform
       rule(:drop => subtree(:tree)) { }
-      rule(:nr => simple(:nr), :name => simple(:name), :result => simple(:result), :time => simple(:time) ) do
-        { :nr => Integer(nr), :name => String(name), :result => String(result).downcase!, :time => Float(time) }
+      rule(:nr => simple(:nr), :total_nr => simple(:total_nr), :name => simple(:name), :result => simple(:result), :time_sec => simple(:time_sec) ) do
+        { :nr => Integer(nr), :total_nr => Integer(total_nr), :name => String(name), :result => String(result).downcase!.to_sym, :time_sec => Float(time_sec) }
       end
       rule(:array => subtree(:tree)) { tree.is_a?(Array) ? tree.compact : [ tree ]  }
     end # class Transform
   end # module CTestStdout
 
+  module CTestLog
+    class Parser < Parslet::Parser
+      rule(:space)     { match['\s'] }
+      rule(:space?)    { space.repeat }
+
+      rule(:newline)   { str("\r").maybe >> str("\n") }
+      rule(:restofline){ ( newline.absent? >> any ).repeat }
+      rule(:anyline) do
+        restofline >> newline |
+        ( newline.absent? >> any ).repeat(1)
+      end
+
+      rule(:letters)   { match['[:alnum:]'].repeat(1) }
+      rule(:path)      { match['[:alnum:]0-9\+\.\-_/'].repeat(1) }
+      rule(:integer)   { match['0-9'].repeat(1) }
+      rule(:float)     { integer >> (match['\.,'] >> integer).maybe }
+
+      rule(:endofoutput) { str('<end of output>') >> newline }
+      rule(:output)      { ( endofoutput.absent? >> any ).repeat }
+
+      rule(:endtime) { str('end time: ') >> restofline.as(:endtime) }
+
+      rule(:event) do
+        integer.as(:nr) >> str('/') >> integer.as(:total_nr) >> str(' Test: ') >> path.as(:name) >> newline >>
+        str('Command: "')  >> path.as(:command) >> str('"') >> newline >>
+        str('Directory: ') >> path.as(:directory) >> newline >>
+        str('"') >> path >> str('" start time: ') >> restofline.as(:starttime) >> newline >>
+        str('Output:') >> newline >>
+        str('----------------------------------------------------------') >> newline >>
+        output.as(:output) >>
+        endofoutput >>
+        str('Test time =')>> space? >> float.as(:time_sec) >> str(' sec') >> newline >>
+        str('----------------------------------------------------------') >> newline >>
+#        str('Test Passed.') >> newline >>
+        str('Test ') >> letters.as(:result) >>
+        ( endtime.absent? >> any ).repeat >> endtime >> newline >>
+        str('"') >> path >> str('" time elapsed: ') >> restofline.as(:time) >> newline >>
+        str('----------------------------------------------------------') >> newline
+      end
+
+      rule(:start) { (event | anyline.as(:drop)).repeat.as(:array) }
+      root(:start)
+    end # class Parser
+
+    class Transform < Parslet::Transform
+      rule(:drop => subtree(:tree)) { }
+      rule(:nr => simple(:nr), :total_nr => simple(:total_nr), :name => simple(:name), :command => simple(:command), :directory => simple(:directory), :starttime => simple(:starttime), :output => simple(:output), :time_sec => simple(:time_sec), :result => simple(:result), :endtime => simple(:endtime), :time => simple(:time) ) do
+        if result == 'Pass' || result == 'Passed'
+          result = :passed
+        else
+          result = :failed
+        end
+        { :nr => Integer(nr), :total_nr => Integer(total_nr), :name => String(name), :command => String(command), :directory => String(directory), :starttime => String(starttime), :output => String(output), :time_sec => Float(time_sec), :result => result, :endtime => String(endtime), :time => String(time) }
+      end
+      rule(:nr => simple(:nr), :total_nr => simple(:total_nr), :name => simple(:name), :command => simple(:command), :directory => simple(:directory), :starttime => simple(:starttime), :output => sequence(:output), :time_sec => simple(:time_sec), :result => simple(:result), :endtime => simple(:endtime), :time => simple(:time) ) do
+        if result == 'Pass' || result == 'Passed'
+          result = :passed
+        else
+          result = :failed
+        end
+        { :nr => Integer(nr), :total_nr => Integer(total_nr), :name => String(name), :command => String(command), :directory => String(directory), :starttime => String(starttime), :output => "", :time_sec => Float(time_sec), :result => result, :endtime => String(endtime), :time => String(time) }
+      end
+      rule(:array => subtree(:tree)) { tree.is_a?(Array) ? tree.compact : [ tree ]  }
+    end # class Transform
+  end # module CTestLog
 
   class CTestParser < BuildLogParser::Parser
     attr_reader :data
     attr_reader :errors
-    attr_reader :test_count
 
     def reset()
       super()
       @data       = []
       @errors     = 0
-      @test_count = 0
-
     end
 
     def parseStdout(logtext)
@@ -57,8 +119,19 @@ module BuildLogParser
       @data = CTestStdout::Transform.new.apply(tree)
 
       @data.each do |event|
-        @test_count += 1
-        @errors     += 1 unless event[:result].to_sym == :passed
+        @errors += 1 unless event[:result] == :passed
+      end
+    end
+
+    def parseLog(logtext)
+      reset()
+      @logtext = logtext
+      parser = CTestLog::Parser.new
+      tree = parser.parse(logtext)
+      @data = CTestLog::Transform.new.apply(tree)
+
+      @data.each do |event|
+        @errors += 1 unless event[:result] == :passed
       end
     end
   end # class CTestParser
--- test/test_ctest.rb
+++ test/test_ctest.rb
@@ -4,6 +4,28 @@ require 'buildlogparser'
 require 'pp'
 
 class CTestTest < Minitest::Test
+  def test_stdout_success
+    parser = BuildLogParser::CTestParser.new()
+
+    logtext = IO.read(File.expand_path('../ctest/stdout_success.txt', __FILE__))
+    parser.parseStdout(logtext)
+    assert_equal logtext, parser.logtext
+    assert_equal 0, parser.errors
+    assert_equal 6, parser.data.size()
+
+    assert_equal 1, parser.data[0][:nr]
+    assert_equal 6, parser.data[0][:total_nr]
+    assert_equal "simpleadd", parser.data[0][:name]
+    assert_equal :passed, parser.data[0][:result]
+    assert_equal 0.0, parser.data[0][:time_sec]
+
+    assert_equal 6, parser.data[5][:nr]
+    assert_equal 6, parser.data[5][:total_nr]
+    assert_equal "coremark", parser.data[5][:name]
+    assert_equal :passed, parser.data[5][:result]
+    assert_equal 14.16, parser.data[5][:time_sec]
+  end
+
   def test_stdout_timeout
     parser = BuildLogParser::CTestParser.new()
 
@@ -11,9 +33,67 @@ class CTestTest < Minitest::Test
     parser.parseStdout(logtext)
     assert_equal logtext, parser.logtext
     assert_equal 1, parser.errors
-    assert_equal 20, parser.test_count
+    assert_equal 20, parser.data.size()
+
+    assert_equal 1, parser.data[0][:nr]
+    assert_equal 20, parser.data[0][:total_nr]
     assert_equal "RV32IMXIE_CF_endless_loop", parser.data[0][:name]
-    assert_equal "timeout", parser.data[0][:result]
-    assert_equal 5.0, parser.data[0][:time]
+    assert_equal :timeout, parser.data[0][:result]
+    assert_equal 5.0, parser.data[0][:time_sec]
+  end
+
+  def test_log_success
+    parser = BuildLogParser::CTestParser.new()
+
+    logtext = IO.read(File.expand_path('../ctest/log_success.log', __FILE__))
+    parser.parseLog(logtext)
+    assert_equal logtext, parser.logtext
+    assert_equal 0, parser.errors
+    assert_equal 2, parser.data.size()
+
+    assert_equal 4, parser.data[0][:nr]
+    assert_equal 6, parser.data[0][:total_nr]
+    assert_equal "indcall", parser.data[0][:name]
+    assert_equal "/home/mwerner/Projekte/sce/test_programs/_build/src/indcall", parser.data[0][:command]
+    assert_equal "/home/mwerner/Projekte/sce/test_programs/_build/src", parser.data[0][:directory]
+    assert_equal "Jul 24 08:41 CEST", parser.data[0][:starttime]
+    assert_equal "textA\ntextB\n", parser.data[0][:output]
+    assert_equal 1.12, parser.data[0][:time_sec]
+    assert_equal :passed, parser.data[0][:result]
+    assert_equal "Jul 24 08:42 CEST", parser.data[0][:endtime]
+    assert_equal "00:00:01", parser.data[0][:time]
+
+    assert_equal 5, parser.data[1][:nr]
+    assert_equal 6, parser.data[1][:total_nr]
+    assert_equal "aes", parser.data[1][:name]
+    assert_equal "/home/mwerner/Projekte/sce/test_programs/_build/src/aes", parser.data[1][:command]
+    assert_equal "/home/mwerner/Projekte/sce/test_programs/_build/src", parser.data[1][:directory]
+    assert_equal "Jul 24 08:42 CEST", parser.data[1][:starttime]
+    assert_equal 2.34, parser.data[1][:time_sec]
+    assert_equal :passed, parser.data[1][:result]
+    assert_equal "Jul 24 08:44 CEST", parser.data[1][:endtime]
+    assert_equal "00:00:02", parser.data[1][:time]
+  end
+
+  def test_log_success_no_output
+    parser = BuildLogParser::CTestParser.new()
+
+    logtext = IO.read(File.expand_path('../ctest/log_success_no_output.log', __FILE__))
+    parser.parseLog(logtext)
+    assert_equal logtext, parser.logtext
+    assert_equal 0, parser.errors, 0
+    assert_equal 3, parser.data.size()
+
+    assert_equal 1, parser.data[0][:nr]
+    assert_equal 6, parser.data[0][:total_nr]
+    assert_equal "simpleadd", parser.data[0][:name]
+    assert_equal "/home/mwerner/Projekte/sce/test_programs/_build/src/simpleadd", parser.data[0][:command]
+    assert_equal "/home/mwerner/Projekte/sce/test_programs/_build/src", parser.data[0][:directory]
+    assert_equal "Jul 24 08:43 CEST", parser.data[0][:starttime]
+    assert_equal "", parser.data[0][:output]
+    assert_equal 0.0, parser.data[0][:time_sec]
+    assert_equal :passed, parser.data[0][:result]
+    assert_equal "Jul 24 08:43 CEST", parser.data[0][:endtime]
+    assert_equal "00:00:00", parser.data[0][:time]
   end
 end
