5a07ea50808ddacba0ae741e08e03db86d4fbe23
Mario Werner <mario.werner@iaik.tugraz.at>
2018-05-11 10:12:06 +0200

    bump version for the new development branch

--- lib/buildlogparser/version.rb
+++ lib/buildlogparser/version.rb
@@ -21,9 +21,9 @@
 module BuildLogParser
   module VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 3
-    PATCH = 1
+    MINOR = 4
+    PATCH = 0
 
-    STRING = [MAJOR, MINOR, PATCH].join('.')
+    STRING = [MAJOR, MINOR, PATCH].join('.') + '-dev'
   end
 end
